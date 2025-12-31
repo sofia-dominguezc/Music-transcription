48bc22aab620f977ac6a8412ec206b50f5220818
Sofia <sofia.domingc@gmail.com>
2025-12-30 23:36:00 -0500

    best model so far: CNN in time + Transformer in frequency

--- README.md
+++ README.md
@@ -126,10 +126,20 @@ This worked equally well with `filter_scale=0.25` than with `0.5`
 
 - Third model: `dim=192` and `3.2M` parameters. Achieved acc `32.5` (~`43.3` only note names) by epoch 12, and 30% at epoch 7
 
-Some conclusions:
+I tried the same model but using [0.25 octaves, 0.25 seconds] blocks and criss-cross attention.
 
-- Validation loss got stuck at the same value as before. This is definitely a data problem, I think the Q-transform looses too much information.
+- `dim=96` and `1M` parameter model achieves `37.2` (~`50%` only note names) by epoch 19.
 
-- I think training was faster here (19% epoch 3, 25% epoch 7, 29% epoch 14, and then stuck for 25 more epochs)
+![alt text](images/test_results.png)
 
-- It's time to move on to time-based methos
+Something interesting is that models that perform better do so even if the training loss is higher. They generalize better.
+
+### Best architecture
+
+It's a combination of transformers and CNNs. Take the input (time, freq, dim), apply 1D convolutions in the time axis and self-attention in the frequency axis.
+
+I use a 5 second window with 8 samples per note. The patch size is [0.41 seconds, 0.25 octaves]
+
+- `dim=96`, `depth=3`, `400K` parameter model achieves `43.5%` accuracy 
+
+![alt text](images/test_results_2.png)
--- src/training.py
+++ src/training.py
@@ -1,4 +1,5 @@
 from math import log
+from typing import Any
 import lightning as pl
 from einops import rearrange
 
@@ -16,34 +17,32 @@ class TransformerLayer(nn.Module):
         self.dim = dim
         self.num_heads = num_heads
         self.head_dim = dim // num_heads
+        hidden_dim = int(dim * mlp_ratio)
         assert dim % num_heads == 0, "dim must be divisible by num_heads"
 
         self.norm1 = nn.LayerNorm(dim)
-        self.attn1 = nn.MultiheadAttention(dim, num_heads, batch_first=True)
-        self.attn2 = nn.MultiheadAttention(dim, num_heads, batch_first=True)
+        self.attn = nn.MultiheadAttention(dim, num_heads, batch_first=True)
         self.norm2 = nn.LayerNorm(dim)
         self.mlp = nn.Sequential(
-            nn.Linear(dim, int(dim * mlp_ratio)),
+            nn.Conv1d(dim, hidden_dim, kernel_size=1),
             nn.GELU(),
-            nn.Linear(int(dim * mlp_ratio), dim),
+            nn.Conv1d(hidden_dim, hidden_dim, kernel_size=3, padding=1, groups=hidden_dim),
+            nn.Conv1d(hidden_dim, dim, kernel_size=1),
         )
 
     def forward(self, x):
         batch, seq1, seq2, dim = x.shape
         x_norm = self.norm1(x)
 
-        x1 = rearrange(x_norm, "b t f d -> (b f) t d")
-        attn_out1, _ = self.attn1(x1, x1, x1)
-        attn_out1 = attn_out1.unflatten(0, (batch, seq2)).transpose(1, 2)
-
-        x2 = rearrange(x_norm, "b t f d -> (b t) f d")
-        attn_out2, _ = self.attn1(x2, x2, x2)
-        attn_out2 = attn_out2.unflatten(0, (batch, seq1))
-
-        x = x + attn_out1 + attn_out2
+        xf = rearrange(x_norm, "b t f d -> (b t) f d")
+        attn_out, _ = self.attn(xf, xf, xf)
+        attn_out = attn_out.unflatten(0, (batch, seq1))
+        x = x + attn_out
 
         x_norm = self.norm2(x)
+        x_norm = rearrange(x_norm, "b t f d -> (b f) d t")
         mlp_out = self.mlp(x_norm)
+        mlp_out = rearrange(mlp_out.unflatten(0, (batch, seq2)), "b f d t -> b t f d")
         x = x + mlp_out
 
         return x
@@ -51,11 +50,9 @@ class TransformerLayer(nn.Module):
 
 class MusicTranscription(nn.Module):
     """
-    ViT with (deep) convolutional encoder-decoder
-    NOTE: the time and frequency resolutions are hard-coded
-        at hop_size=256, bins_per_note=8
+    ViT in frequency + CNN in time
     """
-    def __init__(self, dim=96, n_heads=3, depth=3, n_octaves=8):
+    def __init__(self, dim=96, n_heads=3, depth=3):
         super().__init__()
         self.encoder = nn.Sequential(  # (time, freq) -> (time//12, freq//24)
             nn.Conv2d(1, dim//4, kernel_size=5, stride=3),
@@ -74,16 +71,14 @@ class MusicTranscription(nn.Module):
 
         self.pos: torch.Tensor
         self.register_buffer("pos", self._make_pos_encoding(dim=dim, max_len=64))
-        self.n_octaves = n_octaves
 
     def _make_pos_encoding(self, dim, max_len):
-        pe = torch.zeros(max_len, max_len, dim)
-        t = torch.arange(0, max_len, dtype=torch.float)[:, None, None]
-        s = torch.arange(0, max_len, dtype=torch.float)[None, :, None]
+        pe = torch.zeros(max_len, dim)
+        t = torch.arange(0, max_len, dtype=torch.float)[:, None]
         div_term = torch.exp(torch.arange(0, dim, 2).float() * (-log(10000.0) / dim))
 
-        pe[:, :, 0::2] = torch.sin(t * div_term + s * div_term)
-        pe[:, :, 1::2] = torch.cos(t * div_term + s * div_term)
+        pe[:, 0::2] = torch.sin(t * div_term)
+        pe[:, 1::2] = torch.cos(t * div_term)
         return pe
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -94,11 +89,11 @@ class MusicTranscription(nn.Module):
         x = self.encoder(x)
         batch, _, time, freq = x.shape
 
-        x = rearrange(x, "b c t f -> b t f c")
-        x = x + self.pos[None, :time, :freq, :]
+        x = rearrange(x, "b d t f -> b t f d")
+        x = x + self.pos[None, None, :freq, :]
         x = self.model(x)
 
-        x = rearrange(x, "b t f c -> b c t f")
+        x = rearrange(x, "b t f d -> b d t f")
         x = self.decoder(x)
         return x.squeeze(1)
 
@@ -121,7 +116,7 @@ class LitMusicModel(pl.LightningModule):
         self.scheduler = scheduler
         self.allowed_errors = allowed_errors
         self.params_root = params_root
-        self.best_train_loss = float('inf')
+        self.best_val_acc = 0.0
 
     def configure_optimizers(self):  # type: ignore
         return {
@@ -150,9 +145,6 @@ class LitMusicModel(pl.LightningModule):
     def on_train_epoch_end(self):
         loss = self.trainer.callback_metrics['loss_epoch']
         print(f"\nEpoch {self.current_epoch} - train_loss: {loss:.4f}")
-        if loss < self.best_train_loss:
-            torch.save(self.model.state_dict(), f"{self.params_root}\\{dev_model_weights}")
-            self.best_train_loss = loss.item()
 
     def _acc(
         self, label: Tensor, pred: Tensor, e: int = 0,
@@ -172,6 +164,12 @@ class LitMusicModel(pl.LightningModule):
         acc = self._acc(label=y.bool(), pred=(logits >= 0), e=0)
         self.log("val_loss", loss, on_epoch=True, prog_bar=True)
         self.log("val_acc", acc, on_epoch=True, prog_bar=True)
+
+    def on_validation_epoch_end(self) -> None:
+        acc = self.trainer.callback_metrics['val_acc']
+        if acc > self.best_val_acc:
+            torch.save(self.model.state_dict(), f"{self.params_root}\\{dev_model_weights}")
+            self.best_val_acc = acc.item()
 
     def test_step(
         self, batch: tuple[Tensor, ...], batch_idx: int,
@@ -206,7 +204,7 @@ def train(
     params_root: str = ".",
 ) -> None:
     optimizer = optim.AdamW(model.parameters(), lr=lr, weight_decay=0.01*lr)
-    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, total_epochs, eta_min=0.01*lr)
+    scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, total_epochs, eta_min=0.03*lr)
     plmodel = LitMusicModel(model, optimizer, scheduler, params_root=params_root)
     trainer = pl.Trainer(max_epochs=total_epochs, logger=False, enable_checkpointing=False)
     trainer.fit(plmodel, train_loader, val_loader)
@@ -244,16 +242,16 @@ if __name__ == "__main__":
     torch.set_float32_matmul_precision('medium')
     from dataloaders import create_lazy_dataloader
 
-    model = MusicTranscription(dim=96, n_heads=3, depth=3, n_octaves=8)
-    # model.load_state_dict(torch.load("parameters\\model_weights.pth"))
-    train_loader = create_lazy_dataloader(split="train", batch_size=64, num_workers=8)
+    model = MusicTranscription(dim=192, n_heads=6, depth=6)
+    model.load_state_dict(torch.load("parameters\\model_weights.pth"))
+    train_loader = create_lazy_dataloader(split="train", batch_size=32, num_workers=8)
     val_loader = create_lazy_dataloader(split="test", batch_size=16, num_workers=0)
 
     train(
         model,
         train_loader,
         lr=3e-3,
-        total_epochs=30,
+        total_epochs=40,
         val_loader=val_loader,
     )
 
